f314284f924d47678a08785262b18a13b45f8630
Vu Dinh Truong <vutruong.itsoft@gmail.com>
2020-03-29 15:08:43 +0700

    config db

--- public/index.php
+++ public/index.php
@@ -1,5 +1,11 @@
 <?php
 
+$database_host = 'localhost';
+$database_user = 'root';
+$database_password = 'truongvd@130194';
+$database_name = 'laravel';
+$connection = mysqli_connect($database_host,$database_user,$database_password,$database_name);
+var_dump($connection);die;
 /**
  * Laravel - A PHP Framework For Web Artisans
  *

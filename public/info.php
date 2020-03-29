fcbb83d27c4b04b6d409efe1d9c33334645b5eab
Vu Dinh Truong <vutruong.itsoft@gmail.com>
2020-03-29 15:12:11 +0700

    config db

--- public/info.php
+++ public/info.php
@@ -4,4 +4,4 @@ $database_user = 'root';
 $database_password = 'truongvd@130194';
 $database_name = 'laravel';
 $connection = mysqli_connect($database_host,$database_user,$database_password,$database_name);
-var_dump($connection);die;
+var_dump(phpinfo());die;

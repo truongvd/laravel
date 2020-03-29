a89a84f1533d912fb726dbda0d35c2284a5248f6
Vu Dinh Truong <vutruong.itsoft@gmail.com>
2020-03-29 19:15:32 +0700

    config db

--- public/info.php
+++ public/info.php
@@ -1,18 +1,19 @@
 <?php
-
 // On WEB_SERVER
 $host="ec2-3-20-174-133.us-east-2.compute.amazonaws.com";              // Host name
 //$host="3.20.174.133";              // Host name
-$username="admintruongvd";    // Mysql usernam
+$username="admin";    // Mysql usernam
 $password="Truong@1";           // Mysql password
-$db_name="truongvd";             // Database name
+$db_name="laravel";             // Database name
 $db_port="3306";
 
-$mysqli = new mysqli($host,$username,$password,$db_name);
+$mysqli = new mysqli($host,$username,$password,$db_name, $db_port);
 
 // Check connection
 if ($mysqli -> connect_errno) {
   echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
   exit();
+} else {
+    echo 'connect success';
 }
 ?>

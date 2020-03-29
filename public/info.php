2be17811286c2d088e6bfbc534528eb7b58d486a
Vu Dinh Truong <vutruong.itsoft@gmail.com>
2020-03-29 17:50:15 +0700

    config db

--- public/info.php
+++ public/info.php
@@ -1,7 +1,18 @@
 <?php
-$database_host = 'localhost';
-$database_user = 'root';
-$database_password = '';
-$database_name = '';
-$connection = mysqli_connect($database_host,$database_user,$database_password,$database_name);
-echo $connection;
+
+// On WEB_SERVER
+$host="ec2-3-20-174-133.us-east-2.compute.amazonaws.com";              // Host name
+//$host="3.20.174.133";              // Host name
+$username="admintruongvd";    // Mysql usernam
+$password="Truong@1";           // Mysql password
+$db_name="truongvd";             // Database name
+$db_port="3306";
+
+$mysqli = new mysqli($host,$username,$password,$db_name);
+
+// Check connection
+if ($mysqli -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
+}
+?>
